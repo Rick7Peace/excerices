237a44acabb01517af204db0cf5db24aa920be86
ricardo marmolejo <marmolejo.ricardo@gmail.com>
2025-08-09 01:43:25 -0400

    Modify powerball folder, html, index.js

--- powerball/assets/index.js
+++ powerball/assets/index.js
@@ -1,6 +1,23 @@
-function getPowerball() {
-  // This function simulates getting a Powerball number
+const RED_MAX = 26;
+const WHITE_MAX = 69;
 
-  document.getElementById("powerball").innerHTML = "Your Powerball: 26";
+function getBall(maxValue) {
+  return Math.ceil(Math.random()*maxValue);
+}
+
+function getPowerball() {
+  var redBall = getBall(RED_MAX);
+  var whiteBalls = [];
+  for (let i = 0; i < 5; i++) {
+    var whiteBall = getBall(WHITE_MAX);
+    whiteBalls.push(whiteBall);
+  }
+
+    console.log(whiteBalls.length);
+    console.log(whiteBalls[0]);
+
+  document.getElementById("redball").innerHTML = "Your Powerball: " + redBall;
+  document.getElementById("whiteball-one").innerHTML =
+    "Your Powerball:" + whiteBalls;
 }
 
